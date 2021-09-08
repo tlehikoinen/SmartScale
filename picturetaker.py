8a79301533f1cfdd6d47c150cfe0c96fa16c4ebc
Tommi <akuankka75@gmail.com>
2021-09-08 11:54:55 +0300

    picturetaker updated, train and test started

--- picturetaker.py
+++ picturetaker.py
@@ -2,23 +2,24 @@ import cv2
 import uuid
 import os
 import time
+import sys
 
-
-IMAGES_PATH = os.path.join(os.getcwd(), 'images', 'webcamimages')
+amount = sys.argv[1]
+IMAGES_PATH = os.path.join(os.getcwd(), 'images', 'testimages')
 
 if not os.path.exists(IMAGES_PATH):
     os.makedirs(IMAGES_PATH)
 
 cap = cv2.VideoCapture(0)
+for i in range (0,int(amount)):
+    for i in range(3,0,-1):
+        print("Taking picture " + str(i) + "s")
+        time.sleep(1)
 
-for i in range(3,0,-1):
-    print("Taking picture " + str(i) + "s")
-    time.sleep(1)
-
-ret, frame = cap.read()
-imgname = os.path.join(IMAGES_PATH, str(uuid.uuid1()) + '.jpg')
-print(imgname)
-cv2.imwrite(imgname, frame)
-cv2.imshow('frame', frame)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    ret, frame = cap.read()
+    imgname = os.path.join(IMAGES_PATH, str(uuid.uuid1()) + '.jpg')
+    print(imgname)
+    cv2.imwrite(imgname, frame)
+    cv2.imshow('frame', frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- readme.md
+++ readme.md
@@ -3,5 +3,7 @@
 1. py -m pip install virtualenv  
 2. py -m venv objdetect
 3. objdetect\Scripts\activate  
-4. py -m pip install -r requirements.txt
+4. py -m pip install -r requirements.txt  
+5. py -m ipykernel install --user --name=objdetect
+
 
